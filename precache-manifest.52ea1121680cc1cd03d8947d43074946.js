a380f6ca31a532d9e36574410ec6901f91b8201a
Raviezz <projectworkkmit@gmail.com>
2020-04-17 08:12:34 -0700

    Updates

---
+++ precache-manifest.52ea1121680cc1cd03d8947d43074946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405dacc19ed62b4ddf00a7542637a627",
+    "url": "/tech-blogger/index.html"
+  },
+  {
+    "revision": "d5709e2e3c4322b9ef9e",
+    "url": "/tech-blogger/static/css/2.4cac4a78.chunk.css"
+  },
+  {
+    "revision": "d770744f5fa6c88b241a",
+    "url": "/tech-blogger/static/css/main.796edb9e.chunk.css"
+  },
+  {
+    "revision": "d5709e2e3c4322b9ef9e",
+    "url": "/tech-blogger/static/js/2.c7c0ffcc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/tech-blogger/static/js/2.c7c0ffcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d770744f5fa6c88b241a",
+    "url": "/tech-blogger/static/js/main.27cb0162.chunk.js"
+  },
+  {
+    "revision": "863847df7a5f58a0e4f2",
+    "url": "/tech-blogger/static/js/runtime-main.f18cf033.js"
+  }
+]);
